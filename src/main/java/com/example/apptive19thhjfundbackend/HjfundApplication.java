c7dfefa8eb0d1ae979b3b68ec2c5c34706847198
zigimi <chju0905@pusan.ac.kr>
2023-09-17 23:49:22 +0900

    initial commit

---
+++ src/main/java/com/example/apptive19thhjfundbackend/HjfundApplication.java
@@ -0,0 +1,13 @@
+package com.example.apptive19thhjfundbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjfundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apptive19thhjfundbackend/HjfundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apptive19thhjfundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjfundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
