5a7cb1b7e063405cf576bdc537f877914e31be8b
zigimi <chju0905@pusan.ac.kr>
2024-01-17 20:37:09 +0900

    feat: 메인 코드 머지 후 변경사항 적용

--- src/main/java/com/example/apptive19thhjfundbackend/post/data/entity/Post.java
+++ src/main/java/com/example/apptive19thhjfundbackend/post/data/entity/Post.java
@@ -41,7 +41,7 @@ public class Post extends BaseTimeEntity {
     private int views;
 
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
     @OneToMany(fetch = LAZY, mappedBy = "post", cascade = REMOVE)
