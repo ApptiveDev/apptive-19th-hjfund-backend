eee74dc73cee4e8ed1e1631b114facbb269104b5
인성 (insd) <me@insd.dev>
2024-01-25 14:40:06 +0900

    [NOISSUE] Properties 사용 방법 수정 (#44) (#45)

--- src/main/java/com/example/apptive19thhjfundbackend/user/config/WebMvcConfig.java
+++ src/main/java/com/example/apptive19thhjfundbackend/user/config/WebMvcConfig.java
@@ -5,14 +5,23 @@ import java.util.Properties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration // 스프링 빈으로 등록
 public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
 
+    @Value("${hjfund.deploy.type}")
+    private String deployType;
+
+    @Value("${hjfund.deploy.develop_origin}")
+    private String developOrigin;
+
+    @Value("${hjfund.deploy.main_origin}")
+    private String mainOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        Properties prop = new Properties();
 
         // 모든 경로에 대해
         registry.addMapping("/**")
@@ -22,17 +31,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
 
-        String deployType = prop.getProperty("hjfund.deploy.type");
-
         // deploy.type에 따라 다른 도메인을 허용한다.
         if (deployType.equals("develop")) {
             registry.addMapping("/**")
-                    .allowedOrigins(prop.getProperty("hjfund.deploy.develop_origin"));
+                    .allowedOrigins(developOrigin);
         }
 
         if (deployType.equals("main")) {
             registry.addMapping("/**")
-                    .allowedOrigins(prop.getProperty("hjfund.deploy.main_origin"));
+                    .allowedOrigins(mainOrigin);
         }
     }
 }
