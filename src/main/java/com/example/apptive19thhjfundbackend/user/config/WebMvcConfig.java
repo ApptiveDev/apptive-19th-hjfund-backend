f5ac531d345df6db75a54418246ad7c529f6717d
인성 (insd) <me@insd.dev>
2024-01-25 13:33:53 +0900

    [NOISSUE] CORS Origin Properties (#42)

--- src/main/java/com/example/apptive19thhjfundbackend/user/config/WebMvcConfig.java
+++ src/main/java/com/example/apptive19thhjfundbackend/user/config/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package com.example.apptive19thhjfundbackend.user.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,14 +12,27 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        Properties prop = new Properties();
+
         // 모든 경로에 대해
         registry.addMapping("/**")
-                // Origin이 http:localhost:3000에 대해
-                .allowedOrigins("http://localhost:3000", "https://hjfund.insd.dev")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
+
+        String deployType = prop.getProperty("hjfund.deploy.type");
+
+        // deploy.type에 따라 다른 도메인을 허용한다.
+        if (deployType.equals("develop")) {
+            registry.addMapping("/**")
+                    .allowedOrigins(prop.getProperty("hjfund.deploy.develop_origin"));
+        }
+
+        if (deployType.equals("main")) {
+            registry.addMapping("/**")
+                    .allowedOrigins(prop.getProperty("hjfund.deploy.main_origin"));
+        }
     }
 }
